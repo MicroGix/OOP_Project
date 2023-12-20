49a0d091ddac920ba289aa8760944d026b0fc4af
Thuy Nga <92284690+Schrrodinger@users.noreply.github.com>
2023-12-20 11:16:12 +0700

    check

---
+++ ChickenInvaders/demo/src/main/java/game/demo/Plane.java
@@ -0,0 +1,70 @@
+package game.demo;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class Plane extends Item {
+    public static int score;
+    private GamePane pane;
+    private boolean winner;
+    private boolean alive;
+
+    public Plane(double x, double y, GamePane pane) {
+        super("file:src/images/plane.png", x, y);
+        this.score = 0;
+        this.pane = pane;
+        winner = false;
+        alive = true;
+    }
+
+    public void moveLeft() {
+        setX(getX() - 18);
+    }
+
+    public void moveRight() {
+        setX(getX() + 18);
+    }
+
+    public void moveUp() {
+        setY(getY() - 18);
+    }
+
+    public void moveDown() {
+        setY(getY() + 18 );
+    }
+
+    // score method
+    public int getScore() {
+        return this.score;
+    }
+
+
+    // shot method
+    public void shot() {
+        new Shot(this.getX()+25, this.getY()-25, this.pane);
+    }
+    public void updateScore() {
+        score += 5;
+    }
+    public void die() {
+        if (alive && !winner) {
+            alive = false;
+            pane.getChildren().remove(shape);
+            this.pane.removePlane();
+            pane.gameOver();
+        }
+    }
+    public void hited(){
+        pane.diemtraitim = pane.diemtraitim -1;
+        pane.xoatraitim();
+    }
+
+    public void win() {
+        if (this.getScore()==300){
+            winner = true;
+            pane.gameOver();
+        }
+    }
+}
