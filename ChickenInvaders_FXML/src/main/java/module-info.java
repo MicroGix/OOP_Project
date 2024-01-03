fa8f4d6eb5d524b09ca88f8ce346c1993053c270
MicroGix <138866891+MicroGix@users.noreply.github.com>
2024-01-03 15:59:59 +0700

    Remake game using SceneBuilder

--- ChickenInvaders_FXML/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module project.chickeninvaders {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires java.desktop;
-
-    opens project.chickeninvaders to javafx.fxml;
-    exports project.chickeninvaders;
-}
