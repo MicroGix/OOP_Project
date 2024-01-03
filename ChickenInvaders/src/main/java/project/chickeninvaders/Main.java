00ee1374ca8f801cde13ff1a8623d4277092234c
MicroGix <138866891+MicroGix@users.noreply.github.com>
2024-01-03 16:32:41 +0700

    Update and testing FXML

--- ChickenInvaders/src/main/java/project/chickeninvaders/Main.java
+++ ChickenInvaders/src/main/java/project/chickeninvaders/Main.java
@@ -1,4 +1,25 @@
 package project.chickeninvaders;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage s) throws Exception {
+        Stage stage = MStage.getInstance().loadStage();
+        Scene menu = new MScene("fxml/menu.fxml").loadScene();
+//        Image logo = new Image(getClass().getResource("img/other/logo.png").toExternalForm());
+//        stage.getIcons().add(logo);
+
+        stage.setTitle("Chicken Invaders");
+        stage.setResizable(false);
+        stage.setScene(menu);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
---
+++ ChickenInvaders/src/main/java/project/chickeninvaders/SceneController.java
@@ -0,0 +1,4 @@
+package project.chickeninvaders;
+
+public class SceneController {
+}
