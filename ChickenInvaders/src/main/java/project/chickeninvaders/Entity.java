f49305062d0009a94336c63d76aa8fa14039e89c
Khiem Nguyen <nhatkhiem003@gmail.com>
2024-01-04 15:13:03 +0700

    Update Entity.java

--- ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
+++ ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
@@ -13,3 +13,26 @@ public abstract class Entity {
     public boolean exploding, destroyed;
     protected int explosionStep = 0;
     protected final Image explosionImg = new Image(GameController.class.getResource("img/other/explosion1.png").toString());
+// constructor
+protected Entity(int x, int y, int size, Image img) {
+        this.img = img;
+        this.size = size;
+        this.x = x;
+        this.y = y;
+    }
+// getters and setters
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
