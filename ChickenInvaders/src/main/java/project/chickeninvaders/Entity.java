83d1dfe1c52f494f1548f607eaef15efae68d18c
Khiem Nguyen <nhatkhiem003@gmail.com>
2024-01-04 15:15:23 +0700

    Update Entity.java

--- ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
+++ ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
@@ -36,3 +36,26 @@ protected Entity(int x, int y, int size, Image img) {
     public void setY(int y) {
         this.y = y;
     }
+    public void update() {
+        if (exploding) destroyed=true;
+    }
+
+    public void draw() {
+        if (exploding) { //checking if exploding is true
+            int explosionWidth = 128;
+            int explosionHeight = 128;
+            int explosionRow = 3;
+            int explosionCol = 3;
+            gc.drawImage(explosionImg, explosionStep % explosionCol * explosionWidth,
+                    ((double) explosionStep / explosionRow) * explosionHeight + 1,
+                    explosionWidth, explosionHeight, x, y, size, size);
+        } else {
+            gc.drawImage(img, x, y, size, size);
+        }
+    }
+
+    public void explode() {
+        exploding = true;
+        explosionStep = -1;
+    }
+}
