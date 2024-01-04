f1ca3e1593dfa5ff318d10f2172a8f049a3cde20
Khiem Nguyen <nhatkhiem003@gmail.com>
2024-01-04 15:08:49 +0700

    Create entity class

---
+++ ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
@@ -0,0 +1,5 @@
+package project.chickeninvaders.entities;
+
+import javafx.scene.image.Image;
+import project.chickeninvaders.GameController;
+import static project.chickeninvaders.GameController.*;
