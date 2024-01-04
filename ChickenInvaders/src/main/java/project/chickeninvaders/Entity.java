3ade60303b2d2a7373ee7b59c8f6a20f187a21d2
Khiem Nguyen <nhatkhiem003@gmail.com>
2024-01-04 15:11:31 +0700

    Update Entity.java

--- ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
+++ ChickenInvaders/src/main/java/project/chickeninvaders/Entity.java
@@ -1,5 +1,15 @@
+// import needed packages
 package project.chickeninvaders.entities;
 
 import javafx.scene.image.Image;
 import project.chickeninvaders.GameController;
 import static project.chickeninvaders.GameController.*;
+// list all the variables and constants
+public abstract class Entity {
+    protected Image img;
+    protected int x;
+    protected int y;
+    protected int size;
+    public boolean exploding, destroyed;
+    protected int explosionStep = 0;
+    protected final Image explosionImg = new Image(GameController.class.getResource("img/other/explosion1.png").toString());
